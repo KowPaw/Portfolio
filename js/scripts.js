a6d4a834968fad6a3ef5d94f57e9834ce1fcbcca
Pawel Kowaliszyn <pkowaliszyn@onet.eu>
2018-01-16 17:53:47 +0100

    Add english version

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,12 @@ $(function(){
 	var language = $('.cv #language .media');
 	var cv = $('.cv .container');
 	var about = 0;
+	var schoolSwitch = 0;
+	var languageSwitch = 0;
+
+	$('nav .navbar-right .btn').click(function(){
+		$('#ver')[0].click();
+	});
 
 	$('.jumbotron a').click(function(){
 		if (about == 0) {
@@ -21,31 +27,47 @@ $(function(){
 			cv.addClass('show');
 			this.innerText = 'zamknij CV';
 		}
-		else {
+		else if (this.innerText == 'zamknij CV') {
 			cv.removeClass('show');
 			this.innerText = 'zobacz CV';
 		}
+		else if (this.innerText == 'watch CV') {
+			cv.addClass('show');
+			this.innerText = 'close CV';
+		}
+		else {
+			cv.removeClass('show');
+			this.innerText = 'watch CV';
+		}		
 	});
 
 	$('.cv #school a').click(function(){
-		if (this.innerText == 'więcej') {
+		if (schoolSwitch == 0) {
 			school.addClass('show');
-			this.innerText = 'mniej';
+			$('.cv #school .glyphicon').removeClass('glyphicon-menu-down');
+			$('.cv #school .glyphicon').addClass('glyphicon-menu-up');
+			schoolSwitch = 1;
 		}
 		else {
 			school.removeClass('show');
-			this.innerText = 'więcej';
+			$('.cv #school .glyphicon').removeClass('glyphicon-menu-up');
+			$('.cv #school .glyphicon').addClass('glyphicon-menu-down');
+			schoolSwitch = 0;
 		}
 	});
 
 	$('.cv #language .media a').click(function(){
-		if (this.innerText == 'więcej') {
+		if (languageSwitch == 0) {
 			language.addClass('show');
-			this.innerText = 'mniej';
+			$('.cv #language .glyphicon').removeClass('glyphicon-menu-down');
+			$('.cv #language .glyphicon').addClass('glyphicon-menu-up');
+			languageSwitch = 1;
 		}
 		else {
 			language.removeClass('show');
-			this.innerText = 'więcej';
+			$('.cv #language .glyphicon').removeClass('glyphicon-menu-up');
+			$('.cv #language .glyphicon').addClass('glyphicon-menu-down');
+			languageSwitch = 0;
 		}
 	});
 
